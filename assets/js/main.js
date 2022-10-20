d1fcb2cb71402a4d658f69b99622f325365de737
kritikmodi <kritikmodi@gmail.com>
2022-10-21 00:58:13 +0530

    Add Javascript function for displaying scrollup icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,7 +110,13 @@ window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
