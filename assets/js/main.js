10da5beee16204628efaa39f3db847aa09c8aada
kritikmodi <kritikmodi@gmail.com>
2022-10-21 00:03:25 +0530

    Add scroll sections active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,6 +88,25 @@ const sendEmail = (e) =>{
 contactForm.addEventListener('submit',sendEmail)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
