ad6811f359da8629e3a2b1d99cdc10f0b14682cb
kritikmodi <kritikmodi@gmail.com>
2022-10-21 01:08:49 +0530

    Modify show scrollup icon constraints

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,7 +113,7 @@ window.addEventListener('scroll', scrollActive)
 const scrollUp = () =>{
 	const scrollUp = document.getElementById('scroll-up')
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+	this.scrollY >= 4250 ? scrollUp.classList.add('show-scroll')
 						: scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
