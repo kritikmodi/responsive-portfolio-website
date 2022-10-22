9e23971b82eb0edd3ee99fa8511cff37cd32a0f9
kritikmodi <kritikmodi@gmail.com>
2022-10-23 00:21:36 +0530

    Update light theme icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,7 +129,7 @@ const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'ri-sun-line'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
