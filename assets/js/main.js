860334c01ddb81e36ac373def49740a100bc61f5
kritikmodi <kritikmodi@gmail.com>
2022-11-05 02:01:16 +0530

    Add footer__container reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,7 +165,7 @@ const sr = ScrollReveal({
     // reset: true
 })
 
-sr.reveal(`.home__data, .projects__container`)
+sr.reveal(`.home__data, .projects__container, .footer__container`)
 sr.reveal(`.home__info div`, {delay: 600, origin: 'bottom', interval: 100})
 sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, {origin: 'left'})
 sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`, {origin: 'right'})
