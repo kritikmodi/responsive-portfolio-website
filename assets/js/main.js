c2d4b311dad0ecb96de3940429ec87d7a34d0b59
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2022-11-17 23:00:51 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,7 +112,7 @@ window.addEventListener('scroll', scrollActive)
 const scrollUp = () =>{
 	const scrollUp = document.getElementById('scroll-up')
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 4250 ? scrollUp.classList.add('show-scroll')
+	this.scrollY >= 3250 ? scrollUp.classList.add('show-scroll')
 						: scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
